b6118bbca21cf3cefceccfce9045ecb57595c725
Wamoka <81672345+Wamoka@users.noreply.github.com>
2021-06-17 19:36:22 +0000

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-xxxxxxxxx
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 the function returns nothing
+*/
+int main(void)
+{
+	/*
+	 * prints the required text
+	 */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
