6d4f446e68b8dc08366d32181d217b1cf5a860b7
Wamoka <81672345+Wamoka@users.noreply.github.com>
2021-06-17 19:34:57 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,14 @@
-dddddddd
+#include <stdio.h>
+/**
+* main - entry for  c coding challenge
+* in low level coding section.
+* Return: 0 returns zero
+*/
+int main(void)
+{
+	/**
+	 * This code puts the below line.
+	*/
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
