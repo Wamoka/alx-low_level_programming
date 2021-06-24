3aaad66e3ea3327c17c79c9a2a722679bf299224
Wamoka <elizabethwamoka@gmail.com>
2021-06-23 23:19:50 -0700

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 /**
  * main - tests functions that prints if integer is positive or negative
- * positive_or_negative: test if i is 0
  * Return: 0
  */
 
@@ -11,5 +10,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-	return (0);
+return (0);
 }
