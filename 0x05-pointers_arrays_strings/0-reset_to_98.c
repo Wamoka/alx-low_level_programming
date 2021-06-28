1b5bd1fb893930d2fa24b96efb1e9e69571e5627
Wamoka <81672345+Wamoka@users.noreply.github.com>
2021-06-28 22:59:32 +0300

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int parameter
+ * @n: input integer
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
