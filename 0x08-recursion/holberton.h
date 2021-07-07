741b0b47ea14a2492d31f68231164f3b7774d939
Wamoka <81672345+Wamoka@users.noreply.github.com>
2021-07-07 23:48:04 +0300

    Update holberton.h

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,8 +1,9 @@
-#ifndef _holberton_h_
-#define _holberton_h_
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
-int _putchar(char c);
-void _puts_recursion(char *s)
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
